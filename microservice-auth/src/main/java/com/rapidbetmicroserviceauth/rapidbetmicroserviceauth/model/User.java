b375d4ed62c5b8e823039cc9b048806d9e3b6c2e
Jazthedev <joelpepa2@gmail.com>
2023-09-17 13:30:06 +0200

    add: User entity

---
+++ microservice-auth/src/main/java/com/rapidbetmicroserviceauth/rapidbetmicroserviceauth/model/User.java
@@ -0,0 +1,69 @@
+package com.rapidbetmicroserviceauth.rapidbetmicroserviceauth.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "credit")
+    private Float credit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Float getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Float credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", email='" + email + '\'' + ", lastName='" + password
+                + '\'' + '}';
+    }
+
+    // Default constructor without any arguments
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.credit = 0.0F;
+    }
+}
+
