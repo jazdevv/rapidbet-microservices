432187620ee4ea7d16f67a6af832a73370932f87
Jazthedev <joelpepa2@gmail.com>
2023-09-21 20:19:42 +0200

    fix: ResponseLogin

--- microservice-auth/src/main/java/com/rapidbetmicroserviceauth/rapidbetmicroserviceauth/payload/response/ResponseLogin.java
+++ microservice-auth/src/main/java/com/rapidbetmicroserviceauth/rapidbetmicroserviceauth/payload/response/ResponseLogin.java
@@ -2,8 +2,14 @@ package com.rapidbetmicroserviceauth.rapidbetmicroserviceauth.payload.response;
 
 import com.rapidbetmicroserviceauth.rapidbetmicroserviceauth.model.User;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class ResponseLogin {
     String status;
     String message;
