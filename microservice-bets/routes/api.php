8178ce4a09ecd100fbd80ee502e16c328bb29850
Jazthedev <joelpepa2@gmail.com>
2023-09-25 17:41:12 +0200

    add: admin routes

--- microservice-bets/routes/api.php
+++ microservice-bets/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Bets;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('/v1')->group(function (){
+
+    Route::prefix('/admin')->group(function (){
+        Route::post('/newGame',[Bets::class,'newGame']);
+        Route::post('/newGameBet',[Bets::class,'newGameBet']);
+        Route::post('/setWinnerGameBet',[Bets::class,'setWinnerGameBet']);
+    });
+
+});
+
+
+
