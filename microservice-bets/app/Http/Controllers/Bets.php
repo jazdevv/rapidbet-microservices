0452b324e94a39ee718242e02ec46a5e41055b3b
Jazthedev <joelpepa2@gmail.com>
2023-09-30 10:59:26 +0200

    add: controller newGameBet

--- microservice-bets/app/Http/Controllers/Bets.php
+++ microservice-bets/app/Http/Controllers/Bets.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use App\Models\GameBet;
 use Illuminate\Http\Request;
 
 class Bets extends Controller
@@ -50,7 +51,47 @@ class Bets extends Controller
         }
     }
 
-    public function newGameBet(){
+    public function newGameBet(Request $request){
+        try{
+            $validated = $request->validate([
+                'gameId' => ['required'],
+                'name' => ['required'],
+                'round' => ['required'],
+                'team1name' => ['required'],
+                'team2name' => ['required'],
+                'startDateTimestamp' => ['required'],
+                'endDateTimestamp' => ['required']
+            ]);
+
+            $gameBet = new GameBet();
+            $gameBet -> game_id = $validated['gameId'];
+            $gameBet -> name = $validated['name'];
+            $gameBet -> round = $validated['round'];
+            $gameBet -> team1name = $validated['team1name'];
+            $gameBet -> team2name = $validated['team2name'];
+            $gameBet -> startDateTimestamp = $validated['startDateTimestamp'];
+            $gameBet -> endDateTimestamp = $validated['endDateTimestamp'];
+            $gameBet -> team1amount = 0.0;
+            $gameBet -> team2amount = 0.0;
+            $gameBet -> totalAmount = 0.0;
+            $gameBet -> winner = 0.0;
+            $gameBet -> odd = 0.0;
+
+            $gameBet -> save();
+
+
+            return response()->json([
+                'status' => 'succes',
+                'message' => 'gamebet created correctly',
+                'data' => $gameBet
+            ]);
+        }catch(\Exception $e){
+            return response()->json([
+                'status' => 'fail',
+                'message' => "$e",
+                'data' => ''
+            ]);
+        }
 
     }
 
